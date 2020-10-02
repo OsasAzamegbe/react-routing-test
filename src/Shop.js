a0e40b55cce87444b4ea9571cfbeea1f4f4e58c9
osamudiamen <osas.azamegbe@gmail.com>
2020-10-02 22:15:31 +0100

    rendered out items in shop.js

--- src/Shop.js
+++ src/Shop.js
@@ -2,23 +2,26 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function Shop() {
+  const [items, setItems] = useState([])
 
   useEffect(() =>{
     const fetchItem = async () => {
       const url = 'https://fortnite-api.com/v2/shop/br/combined'
       const response = await fetch(url)
       const data = await response.json()
-      console.log(data)
+      const itemList = data.data.featured.entries
+      console.log(itemList)
+      setItems(itemList)
     }
     fetchItem();
-  }, [])
-
-
-  
+  }, [])  
 
   return (
     <div>
       <h1>Shop</h1>
+      {items.map(item => (
+          <p key={item.offerId} >{item.devName}</p>
+        ))}
     </div>
   );
 }
