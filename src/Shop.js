ccd17a032da2eed60ac42c84159a3d9f67d0d350
osamudiamen <osas.azamegbe@gmail.com>
2020-10-02 21:30:51 +0100

    added useEffect hook and fetch function for shop items.

--- src/Shop.js
+++ src/Shop.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function Shop() {
+
+  useEffect(() =>{
+    const fetchItem = async () => {
+      const url = 'https://fortnite-api.com/v2/shop/br/combined'
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+    }
+    fetchItem();
+  }, [])
+
+
+  
+
   return (
     <div>
       <h1>Shop</h1>
